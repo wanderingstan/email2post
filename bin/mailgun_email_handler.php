43a9bc9c78e1a56eedcf280438cecafd0f39a5f3
wanderingstan <stan@wanderingstan.com>
2013-12-26 16:24:30 -0700

    fixed paths in config. fixed bugs in mailgun handler.

--- bin/mailgun_email_handler.php
+++ bin/mailgun_email_handler.php
@@ -1,6 +1,6 @@
 <?php
 // CGI handler that is called by mailgun when a new email is received
-include_once "../config.php"
+include_once "../config.php";
 
 $mailbox = $_GET['mailbox'];
 
@@ -15,10 +15,10 @@ $out .= "\n<!--END EMAIL-->\n";
 // TODO: save images, add to html
 
 // Save the email contents in staging	
-$filename = '../data/staging/' . date('Y-m-d\TH-i-i\Z' . '.html';
+$filename = '../data/staging/' . date('Y-m-d\TH-i-i\Z') . '.html';
 file_put_contents($filename, $out);
 
 // Log file
-file_put_contents($config['log_file'], "\n---\n" . print_r ($_POST, TRUE), FILE_APPEND | LOCK_EX);		
+file_put_contents($config['LOG_FILE'], "\n---\n" . print_r ($_POST, TRUE), FILE_APPEND | LOCK_EX);		
 
 ?>
