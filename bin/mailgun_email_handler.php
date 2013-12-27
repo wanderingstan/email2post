6b76d06cf88c43e8247c502579bbf851e97f21d6
wanderingstan <stan@wanderingstan.com>
2013-12-26 17:26:38 -0700

    now accepting image attachements

--- bin/mailgun_email_handler.php
+++ bin/mailgun_email_handler.php
@@ -13,13 +13,33 @@ $out .= "<!--BEGIN EMAIL-->\n";
 $out .= $_POST['stripped-html'];
 $out .= "\n<!--END EMAIL-->\n";
 
-// TODO: save images, add to html
-
 // Save the email contents in staging	
-$filename = '../data/staging/' . date('Y-m-d\TH-i-i\Z') . '_' . $_POST['sender'] .'.html';
+
+$filebasename = date('Y-m-d\TH-i-i\Z') . '_' . $_POST['sender'];
+$filename = $config['STAGING_DIR'] . '/'  . $filebasename .'.html';
 file_put_contents($filename, $out);
 
+$log = "";
+
+// TODO: save images, add to html
+$count = 0;
+foreach ($_FILES as $file) {
+	$count++;
+	$attachment_filename = $config['STAGING_DIR'] . '/' . $filebasename . '_' . $count . '_' . basename($file['name']);
+
+	$log .= "Getting attachment {$count} as {$attachment_filename}.\n";
+	// todo test for jpg or other safe image
+
+	if (move_uploaded_file($file['tmp_name'], $attachment_filename)) {
+	    $log .= "File is valid, and was successfully uploaded.\n";
+	} else {
+	    $log .= "Possible file upload attack!\n";
+	}
+}
+
 // Log file
-file_put_contents($config['LOG_FILE'], "\n---\n" . print_r ($_POST, TRUE), FILE_APPEND | LOCK_EX);		
+file_put_contents($config['LOG_FILE'], "\n---\n" . $log . "\n\n" . print_r ($_POST, TRUE), FILE_APPEND | LOCK_EX);		
+
+file_put_contents("most_recent.serialized",serialize($_POST));
 
 ?>
